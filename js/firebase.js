b1d4eb22471927ff01ac3a14c1a27feec4e85a05
Apacargo <hornodulce045@gmail.com>
2025-12-05 17:09:51 -0500

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROJECT_ID.firebaseapp.com",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_PROJECT_ID.appspot.com",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
