7294cb04971e1ef1ffd9f90d15d91f9aad57ff9c
Apacargo <hornodulce045@gmail.com>
2025-12-06 19:03:28 -0500

    Update firebase .js

--- firebase .js
+++ firebase .js
@@ -1,15 +1,20 @@
-// Import Firebase
+// firebase.js
+// Reemplaza las claves y valores por la configuración de tu proyecto Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "TU_APIKEY",
-  authDomain: "TU_AUTH",
+  authDomain: "TU_AUTH_DOMAIN",
   projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE",
-  messagingSenderId: "TU_SENDER",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
   appId: "TU_APPID"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
